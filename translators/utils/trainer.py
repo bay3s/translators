402ab796154bf6826c7985593ba9f6803a613088
bay3s <sirbay3s@gmail.com>
2023-10-01 15:31:57 +0200

    fix bug

--- translators/utils/trainer.py
+++ translators/utils/trainer.py
@@ -137,7 +137,8 @@ class Trainer:
     def run_inference(self, b_source_i):
         with torch.no_grad():
             source_words = []
-            for tok_idx in b_source_i.squeeze().tolist():
+
+            for tok_idx in b_source_i.copy().to("cpu").squeeze().tolist():
                 source_words.append(self.source_vocab.get_itos()[tok_idx])
                 pass
 
